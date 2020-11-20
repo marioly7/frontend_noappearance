61bc8c4216afff44062327be4cf43422a071c2c1
Marioly V <mariolyv7@gmail.com>
2020-11-20 13:48:57 -0400

    Creando carpeta para frontend que contiene proyecto en android con dependencias correspondientes para utilizar retrofit

---
+++ NoAppearanceFrontEnd/app/src/main/java/com/example/noapperance/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.noapperance;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textViewResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
